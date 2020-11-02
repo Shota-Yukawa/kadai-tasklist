fc5866f62f3765f5ccdfd14cbc0f099e1cc3e22e
shota.yukawa <rikanoa315@gmail.com>
2020-11-02 13:54:38 +0000

    CRUD task

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+
+    
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task);
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
+    
+@endsection
