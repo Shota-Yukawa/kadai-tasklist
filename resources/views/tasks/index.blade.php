4056715e2efaf66395e1e1aff8a527c9f30df85d
shota.yukawa <rikanoa315@gmail.com>
2020-11-08 09:48:17 +0000

    heroku complete3

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,25 +1,35 @@
 @extends('layouts.app')
 
 @section('content') 
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>ステータス</th>
-                    <th>タスク</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    @if (Auth::check())
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>ステータス</th>
+                        <th>タスク</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+    @else 
+     <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Tasklist</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
     @endif
     
     {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
